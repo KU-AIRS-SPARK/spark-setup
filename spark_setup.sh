5032707c1c7e95ba878fe283db8830c217338096
Enes Erciyes <merciyes18@ku.edu.tr>
2021-01-27 16:15:29 +0300

    clone simulation utils

--- spark_setup.sh
+++ spark_setup.sh
@@ -2,3 +2,6 @@ sudo apt-get install git
 /bin/bash ./python_installation.sh && /bin/bash ./ros_installation.sh && /bin/bash ./lgsvl_installation.sh && /bin/bash ./pycharm-installation.sh
 
 cp ./initDev.sh ~/spark/
+
+cd ~/spark
+git clone https://github.com/KU-AIRS-SPARK/simulation-utils
