9448e826c5c52c31905e8474d3e11f26de098414
Demet <63194475+dtumkaya@users.noreply.github.com>
2021-01-31 14:52:57 +0300

    Update initDev.sh
    for gnome-terminal not found

--- initDev.sh
+++ initDev.sh
@@ -1,5 +1,6 @@
 source /opt/ros/dashing/setup.bash
 pycharm-community&
 
+sudo apt-get install gnome-terminal
 gnome-terminal --tab -x bash -c 'cd lgsvlsimulator/ros2-lgsvl-bridge&&. install/setup.bash&&lgsvl_bridge;$SHELL'
 gnome-terminal --window -x bash -c 'cd lgsvlsimulator&&./simulator'
