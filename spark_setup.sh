2ff2c1ec28c9f537a6ed66ed49403c9788209a93
Demet <63194475+dtumkaya@users.noreply.github.com>
2021-07-14 14:20:21 +0300

    Update spark_setup.sh

--- spark_setup.sh
+++ spark_setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get install git
-/bin/bash ./python_installation.sh && /bin/bash ./ros_installation.sh && /bin/bash ./lgsvl_installation.sh && /bin/bash ./pycharm-installation.sh
+/bin/bash ./python_installation.sh && /bin/bash ./ros2_installation.sh && /bin/bash ./lgsvl_installation.sh && /bin/bash ./pycharm-installation.sh
 
 cp ./initDev.sh ~/spark/
 
